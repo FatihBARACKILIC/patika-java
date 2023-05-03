1cc91581e5f09d9ff208e646126e04df03984d63
FatihBARACKILIC <fatihb469@gmail.com>
2023-05-03 16:22:49 +0300

    Print A with Array

--- java-101/printAWithArray/src/Main.java
+++ java-101/printAWithArray/src/Main.java
@@ -6,20 +6,17 @@ public class Main {
             for (int j = 0; j < letter[i].length; j++) {
                 if ((i % 3 == 0 || j % 2 == 0) && !(j == 2 && i % 3 == 0)) {
                     letter[i][j] = "*";
-                    System.out.print("*\t");
                 } else {
                     letter[i][j] = " ";
-                    System.out.print(" \t");
-                }p
+                }
+            }
+        }
+
+        for (String[] row : letter) {
+            for (String col : row) {
+                System.out.print(col + "\t");
             }
             System.out.println();
         }
-
-//        for (String[] row : letter) {
-//            for (String col : row) {
-//                System.out.print(col + "\t");
-//            }
-//            System.out.println();
-//        }
     }
 }
