be0cce47d3145b8c19638f8ecec7730e0643ad16
FatihBARACKILIC <fatihb469@gmail.com>
2023-05-24 16:57:24 +0300

    WHERE

--- sql/where.sql
+++ sql/where.sql
@@ -22,3 +22,28 @@ SELECT rental_rate,
     *
 FROM film
 WHERE rental_rate <> 4.99;
+
+SELECT *
+FROM actor
+WHERE first_name = 'Penelope'
+    OR first_name = 'Bob';
+
+SELECT *
+FROM film
+WHERE rental_rate = 4.99
+    OR rental_rate = 0.99;
+
+SELECT title,
+    replacement_cost,
+    rental_rate
+FROM film
+WHERE NOT rental_rate = 4.99
+    AND replacement_cost = 20.99;
+
+SELECT *
+FROM actor
+WHERE NOT(
+        first_name = 'Penelope'
+        OR first_name = 'Nick'
+        OR first_name = 'Ed'
+    );
