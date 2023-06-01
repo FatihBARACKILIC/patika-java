88157c05cfdd95145875d658646e670c8c592eec
FatihBARACKILIC <fatihb469@gmail.com>
2023-06-01 18:37:08 +0300

    INSERT dummy data to authors table

---
+++ sql/insert_dumy_data.sql
@@ -0,0 +1,719 @@
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Sherman',
+        'Freake',
+        'sfreake0@unicef.org',
+        '1915-10-04'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Melosa',
+        'Tzarkov',
+        'mtzarkov1@arstechnica.com',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Allison',
+        'Szymanski',
+        'aszymanski2@patch.com',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Melli',
+        'Ralfe',
+        'mralfe3@imageshack.us',
+        '1904-10-12'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Tamas',
+        'Bohler',
+        'tbohler4@about.me',
+        '1989-06-15'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Felipa',
+        'Lyes',
+        'flyes5@mashable.com',
+        '1978-07-01'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Garrot', 'Lamerton', NULL, '1991-03-18');
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Bax',
+        'Brumby',
+        'bbrumby7@businesswire.com',
+        '1903-11-06'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Gerty', 'Zapata', 'gzapata8@google.co.uk', NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Tine',
+        'Parkins',
+        'tparkins9@tmall.com',
+        '1945-11-30'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Thorvald',
+        'Fisbey',
+        'tfisbeya@surveymonkey.com',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Cornelia', 'Djakovic', NULL, '1974-06-03');
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Dian',
+        'Stopper',
+        'dstopperc@hp.com',
+        '1977-10-26'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Jessalin',
+        'Dinsmore',
+        'jdinsmored@liveinternet.ru',
+        '1914-11-13'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Hamnet',
+        'Worrell',
+        'hworrelle@so-net.ne.jp',
+        '1958-10-28'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Bertram',
+        'Hinemoor',
+        'bhinemoorf@wikispaces.com',
+        '1961-05-04'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Maureene',
+        'Sage',
+        'msageg@hibu.com',
+        '1908-05-08'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Charlotte',
+        'Children',
+        'cchildrenh@whitehouse.gov',
+        '1921-07-11'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Lennard',
+        'Padrick',
+        'lpadricki@indiatimes.com',
+        '1972-06-20'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Kevan',
+        'Neillans',
+        'kneillansj@fda.gov',
+        '1912-12-19'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Chicky',
+        'Surgener',
+        'csurgenerk@typepad.com',
+        '1959-05-13'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Meghann',
+        'Voyce',
+        'mvoycel@businesswire.com',
+        '1932-10-14'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Levi',
+        'Cuddon',
+        'lcuddonm@dyndns.org',
+        '1961-03-28'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Richmound', 'Wondraschek', NULL, NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Hally',
+        'Pidgeley',
+        'hpidgeleyo@macromedia.com',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Zerk',
+        'Jeffress',
+        'zjeffressp@jalbum.net',
+        '1932-04-10'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Hort',
+        'Hawyes',
+        'hhawyesq@hhs.gov',
+        '1973-06-08'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Wilbur',
+        'Budgey',
+        'wbudgeyr@unicef.org',
+        '1998-04-16'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Korrie',
+        'Megarrell',
+        'kmegarrells@clickbank.net',
+        '1931-01-30'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Rafaelita',
+        'Murricanes',
+        'rmurricanest@unesco.org',
+        '1961-11-07'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Casie',
+        'Marquese',
+        'cmarqueseu@topsy.com',
+        '1961-12-21'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Chaddy',
+        'Courtman',
+        'ccourtmanv@theglobeandmail.com',
+        '1951-06-30'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Carleen',
+        'Betty',
+        'cbettyw@seesaa.net',
+        '1957-06-07'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Ryun', 'Atto', 'rattox@marriott.com', NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Rodney',
+        'Mangenot',
+        'rmangenoty@google.fr',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Johnny',
+        'Angove',
+        'jangovez@godaddy.com',
+        '1992-04-08'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Min', 'Roust', NULL, '1946-03-29');
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Dniren',
+        'Dugall',
+        'ddugall11@theguardian.com',
+        '1993-03-01'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Miriam',
+        'Vannucci',
+        'mvannucci12@yahoo.com',
+        '1955-02-10'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Gwenora', 'Sulman', NULL, '1985-03-20');
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Randolf',
+        'Hayford',
+        'rhayford14@cloudflare.com',
+        '1993-05-06'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Adore', 'Buxton', NULL, NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Gisella', 'Truitt', 'gtruitt16@admin.ch', NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Keith',
+        'Loeber',
+        'kloeber17@spotify.com',
+        '1994-11-13'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Enrico',
+        'Lovekin',
+        'elovekin18@cam.ac.uk',
+        '1901-05-20'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Artemus',
+        'Haymes',
+        'ahaymes19@livejournal.com',
+        '1933-06-13'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Micaela',
+        'Owen',
+        'mowen1a@paginegialle.it',
+        '1956-01-23'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Mayor',
+        'Harbar',
+        'mharbar1b@sina.com.cn',
+        '1902-02-27'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Aurthur',
+        'Sigmund',
+        'asigmund1c@1und1.de',
+        '1986-05-30'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Sheree', 'Kinny', NULL, NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Florence',
+        'Unwin',
+        'funwin1e@guardian.co.uk',
+        '1944-10-15'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Lorin',
+        'Lente',
+        'llente1f@craigslist.org',
+        '1903-11-16'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Alair',
+        'Heardman',
+        'aheardman1g@about.com',
+        '1998-04-23'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Andriana',
+        'Swinglehurst',
+        'aswinglehurst1h@ebay.com',
+        '1957-07-30'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Tommie',
+        'Booley',
+        'tbooley1i@amazonaws.com',
+        '1942-08-11'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Saxe',
+        'Macconachy',
+        'smacconachy1j@wikimedia.org',
+        '1943-07-19'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Helena',
+        'Sarney',
+        'hsarney1k@wikia.com',
+        '1934-05-23'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Ayn',
+        'Clemente',
+        'aclemente1l@domainmarket.com',
+        '1937-06-20'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Packston',
+        'Gauthorpp',
+        'pgauthorpp1m@icq.com',
+        '1920-07-24'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Kimberlee',
+        'Plank',
+        'kplank1n@squarespace.com',
+        '1960-01-27'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Farrell',
+        'Holson',
+        'fholson1o@samsung.com',
+        '1923-01-09'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Welby',
+        'Choppen',
+        'wchoppen1p@dailymotion.com',
+        '1952-12-27'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Bartholomeus',
+        'Cecil',
+        'bcecil1q@wix.com',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Shelly',
+        'Sperski',
+        'ssperski1r@bizjournals.com',
+        '1988-04-28'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Jessa',
+        'Bolsteridge',
+        'jbolsteridge1s@alexa.com',
+        '1919-01-13'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Lilla',
+        'Nigh',
+        'lnigh1t@instagram.com',
+        '1921-06-13'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Evaleen', 'Cargo', 'ecargo1u@comcast.net', NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Derrick',
+        'Le Maitre',
+        'dlemaitre1v@com.com',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Darbie',
+        'Carville',
+        'dcarville1w@comcast.net',
+        '1901-09-20'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Jarvis', 'Porkiss', NULL, '1931-05-19');
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Sharron',
+        'Di Batista',
+        'sdibatista1y@marketwatch.com',
+        '1934-11-22'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Dynah',
+        'Tasseler',
+        'dtasseler1z@friendfeed.com',
+        '1974-11-24'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Diannne',
+        'Ragge',
+        'dragge20@cbsnews.com',
+        '1918-06-07'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Cedric',
+        'Duberry',
+        'cduberry21@forbes.com',
+        '1998-07-10'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Hyacinthie',
+        'Fold',
+        'hfold22@purevolume.com',
+        '1971-09-09'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Eran',
+        'Farrington',
+        'efarrington23@who.int',
+        '1903-02-12'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Phil',
+        'Mulhall',
+        'pmulhall24@odnoklassniki.ru',
+        '1980-09-27'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Oneida', 'Postins', NULL, '1949-09-15');
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Bea',
+        'Eykel',
+        'beykel26@shop-pro.jp',
+        '1993-04-08'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Gayleen',
+        'Hyslop',
+        'ghyslop27@yahoo.co.jp',
+        '1902-04-27'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Ulric',
+        'Northcliffe',
+        'unorthcliffe28@xrea.com',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Ly',
+        'Bothams',
+        'lbothams29@dot.gov',
+        '1901-06-15'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Alon',
+        'Kares',
+        'akares2a@aol.com',
+        '1922-01-12'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Tan', 'Hasson', NULL, '1907-09-08');
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Ardeen',
+        'Wymer',
+        'awymer2c@freewebs.com',
+        '1999-05-09'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Allix',
+        'Such',
+        'asuch2d@bloglines.com',
+        '1916-12-31'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Franklin', 'Mac', 'fmac2e@netvibes.com', NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Gardy',
+        'Langshaw',
+        'glangshaw2f@free.fr',
+        '1995-08-23'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Ludovico',
+        'Aykroyd',
+        'laykroyd2g@printfriendly.com',
+        '1934-07-01'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Rhiamon',
+        'Izkoveski',
+        'rizkoveski2h@123-reg.co.uk',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Milo',
+        'Barnwell',
+        'mbarnwell2i@epa.gov',
+        '1953-02-21'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Wylie',
+        'Poultney',
+        'wpoultney2j@creativecommons.org',
+        '1914-04-15'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Randall',
+        'Henrot',
+        'rhenrot2k@freewebs.com',
+        NULL
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Valle',
+        'Widmore',
+        'vwidmore2l@techcrunch.com',
+        '1912-09-24'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Lora',
+        'Joul',
+        'ljoul2m@taobao.com',
+        '1973-05-22'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Berthe',
+        'Blazej',
+        'bblazej2n@boston.com',
+        '1919-04-13'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES ('Jeno', 'Kroon', 'jkroon2o@soup.io', NULL);
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Gratiana',
+        'Nansom',
+        'gnansom2p@cocolog-nifty.com',
+        '1933-01-12'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Malchy',
+        'Feifer',
+        'mfeifer2q@de.vu',
+        '1999-07-02'
+    );
+
+INSERT INTO authors (first_name, last_name, email, birthdate)
+VALUES (
+        'Richart',
+        'Paradin',
+        'rparadin2r@hubpages.com',
+        '1984-10-06'
+    );
